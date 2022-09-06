590c65ec8a834828eba9fd397cd8b042959217d7
aniloncul <aniloncul@gmail.com>
2022-09-06 20:05:43 +0300

    Initial Commit

---
+++ SayacApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayacApp
+//
+//  Created by Anıl Öncül on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
